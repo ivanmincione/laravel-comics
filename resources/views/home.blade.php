5622080f48d742ec7321ac77feeef3e69ca66327
Ivan Mincione <ivan.mincione92@outlook.it>
2021-01-07 17:25:18 +0100

    Add section shop-info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,6 +22,33 @@
                     </div>
                 @endforeach
             </div>
+            <div class="button">
+                <div class="btn-load">
+                    <a class="uppercase" href="#"> load more</a>
+                </div>
+            </div>
+        </div>
+        <div class="shop-link">
+            <div class="card-shop">
+                <img src="/images/buy-comics-digital-comics.png" alt="img DC">
+                <a class="uppercase" href="#">digital comics</a>
+            </div>
+            <div class="card-shop">
+                <img src="/images/buy-comics-merchandise.png" alt="img DC">
+                <a class="uppercase" href="#">DC merchandise</a>
+            </div>
+            <div class="card-shop">
+                <img src="/images/buy-comics-subscriptions.png" alt="img DC">
+                <a class="uppercase" href="#">subscription</a>
+            </div>
+            <div class="card-shop">
+                <img src="/images/buy-comics-shop-locator.png" alt="img DC">
+                <a class="uppercase" href="#">comic shop locator</a>
+            </div>
+            <div class="card-shop">
+                <img src="/images/buy-dc-power-visa.svg" alt="img DC">
+                <a class="uppercase" href="#">dc power visa</a>
+            </div>
         </div>
     </section>
 @endsection
